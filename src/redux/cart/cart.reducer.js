d092aae27ff02fbf4e666950249c44588c9f86e3
alelaru <alelaru@gmail.com>
2020-07-29 00:46:50 -0500

    Updated new shop date, updated cart utils to group items in cart reducer

--- src/redux/cart/cart.reducer.js
+++ src/redux/cart/cart.reducer.js
@@ -1,4 +1,5 @@
 import { cartActionTypes } from "./cart.types";
+import { addItemToCart } from "./cart.utils";
 
 const INITIAL_STATE = {
   hidden: true,
@@ -10,7 +11,10 @@ const cartReducer = (state = INITIAL_STATE, action) => {
     case cartActionTypes.TODDLE_CART_HIDDEN:
       return { ...state, hidden: !state.hidden };
     case cartActionTypes.ADD_ITEM_TO_CART:
-      return { ...state, cartItems: [...state.cartItems, action.payload] };
+      return {
+        ...state,
+        cartItems: addItemToCart(state.cartItems, action.payload),
+      };
     default:
       return state;
   }
---
+++ src/redux/cart/cart.utils.js
@@ -0,0 +1,18 @@
+import { cartActionTypes } from "./cart.types";
+
+// Dunction to not duplicate elements inside the cartActionTypes, checks if its already there and updates the cart or add 1 to the quantity
+export const addItemToCart = (cartItems, cartItemToAdd) => {
+  const existingCartItem = cartItems.find(
+    (item) => item.id === cartItemToAdd.id
+  );
+
+  if (existingCartItem) {
+    return cartItems.map((item) =>
+      item.id === cartItemToAdd.id
+        ? { ...item, quantity: item.quantity + 1 }
+        : item
+    );
+  } else {
+    return [...cartItems, { ...cartItemToAdd, quantity: 1 }];
+  }
+};

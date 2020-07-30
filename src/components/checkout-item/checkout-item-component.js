29fc576983c06a13deab3bf791779a1ce8211505
alelaru <alelaru@gmail.com>
2020-07-29 22:53:22 -0500

    Installed persist package and added to our store to get local storage

--- src/components/checkout-item/checkout-item-component.js
+++ src/components/checkout-item/checkout-item-component.js
@@ -1,19 +1,45 @@
 import React from "react";
 
 import "./checkout-item.scss";
+import { connect } from "react-redux";
+import {
+  removeItemFromCart,
+  removeItem,
+  addItem,
+} from "../../redux/cart/cart.actions";
 
-function CheckoutItem({ cartItem: { name, imageUrl, price, quantity } }) {
+function CheckoutItem({ cartItem, ItemToRemoveFromCart, removeItem, addItem }) {
+  const { name, imageUrl, price, quantity } = cartItem;
   return (
     <div className="checkout-item">
       <div className="image-container">
         <img src={imageUrl} alt="item" />
       </div>
       <span className="name">{name}</span>
-      <span className="quantity">{quantity}</span>
+      <span className="quantity">
+        <div className="arrow" onClick={() => removeItem(cartItem)}>
+          &#10094;
+        </div>
+        <span className="value"> {quantity}</span>
+        <div className="arrow" onClick={() => addItem(cartItem)}>
+          &#10095;
+        </div>
+      </span>
       <span className="price">{price}</span>
-      <span className="remove-button">&#10005;</span>
+      <span
+        className="remove-button"
+        onClick={() => ItemToRemoveFromCart(cartItem)}
+      >
+        &#10005;
+      </span>
     </div>
   );
 }
 
-export default CheckoutItem;
+const mapDispatchTheProps = (dispatch) => ({
+  ItemToRemoveFromCart: (item) => dispatch(removeItemFromCart(item)),
+  addItem: (item) => dispatch(addItem(item)),
+  removeItem: (item) => dispatch(removeItem(item)),
+});
+
+export default connect(null, mapDispatchTheProps)(CheckoutItem);
